0b51b861140caec55252b87872e1d055597e8ab5
kulpunai180 <jekshenbekova@outlook.com>
2025-11-29 18:34:08 +0600

    fix secondary

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -8,25 +8,25 @@ const Button = ({
   onClick,
 }) => {
   const base =
-    "inline-flex items-center justify-center gap-2 rounded-full font-medium text-lg px-8 py-3 transition-all duration-300";
+    "inline-flex items-center justify-center gap-2 font-medium text-lg px-8 py-3 transition-all duration-300";
 
   const variants = {
     primary: `
-      border border-white text-white
-      hover:bg-white hover:text-black
+      border border-white text-white rounded-full
+      hover:bg-white hover:text-black 
       active:scale-95
       disabled:border-[#C5C9D1] disabled:text-[#C5C9D1] disabled:bg-transparent
       disabled:cursor-not-allowed
     `,
     primaryIcon: `
-      border border-white text-white
+      border border-white text-white rounded-full
       hover:bg-white hover:text-black
       active:scale-95
       disabled:border-[#C5C9D1] disabled:text-[#C5C9D1] disabled:bg-transparent
       disabled:cursor-not-allowed
     `,
     secondary: `
-      bg-[#A22220] text-white
+      bg-[#A22220] text-white rounded
       hover:bg-[#751715]
       active:scale-95
       disabled:bg-[#E5E7EB] disabled:text-[#9CA3AF]
