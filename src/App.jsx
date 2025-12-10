041cf5529bdf0c2382dd41d3cbbdb42835c886d5
kulpunai180 <jekshenbekova@outlook.com>
2025-12-10 17:07:31 +0600

    history page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
+import HistoryPage from "./pages/HistoryPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <main className="flex-1">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/history" element={<HistoryPage />} />
           </Routes>
         </main>
         <Footer />
