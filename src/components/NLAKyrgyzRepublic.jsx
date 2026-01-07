4680524a29c1aaefbf51e1ff2090eaf14e6d7150
kulpunai180 <jekshenbekova@outlook.com>
2026-01-07 13:47:20 +0600

    documents

--- src/components/NLAKyrgyzRepublic.jsx
+++ src/components/NLAKyrgyzRepublic.jsx
@@ -1,84 +1,18 @@
 import Button from "../components/UI/Button";
 import { useTranslation } from "react-i18next";
 
-const STATIC_ITEMS = [
-  {
-    text: "Кыргыз Республикасынын Өкмөтүнүн 2011-жылдын 23-августундагы № 496 токтому. Кыргыз Республикасында жогорку кесиптик билим берүүнүн эки деңгээлдүү түзүмүн белгилөө жөнүндө",
-    link: "https://cbd.minjust.gov.kg/7-14864/edition/19609/kg?editionCode=exact",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2024-жылдын 27-сентябрындагы № 590 Жогорку кесиптик билим берүүнүн билим берүү уюмдарынын ишин жөнгө салган ченемдик укуктук актыларды бекитүү жөнүндө токтому.",
-    link: "https://cbd.minjust.gov.kg/7-33050/edition/17445/kg",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2024-жылдын 27-сентябрындагы № 590 токтомуна 1-тиркеме. Академиялык эркиндикти колдоону жогорулатуу кесиптик билим берүү уюмдарына билим берүү программаларын иштеп чыгууга коюлуучу талаптарды белгилөө",
-    link: "https://cbd.minjust.gov.kg/230318483/edition/17447/kg?lang=kg",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2024-жылдын 27-сентябрындагы № 590 токтомуна 2-тиркеме. КРнын жогорку кесиптик билим берүүнүн билим берүү уюмдарынын билим алуучуларын которуу, окуудан чыгаруу, окууга калыбына келтирүү жана академиялык өргүү берүү тартиби жөнүндө ЖОБО.",
-    link: "https://cbd.minjust.gov.kg/230014839/edition/17452/kg?lang=kg",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2024-жылдын 27-сентябрындагы № 590 токтомуна 3-тиркеме. КРнын жогорку кесиптик билим берүүнүн билим берүү уюмдарынын билим алуучуларын учурдагы контролдоо жана орто аралык аттестациялоо жөнүнө ЖОБО",
-    link: "https://cbd.minjust.gov.kg/230051681/edition/17455/kg?lang=kg",
-  },
-  {
-    text: "AIS диплом 2025 санариптик реестри жөнүндө жобо.",
-    link: "https://diplom.edu.gov.kg/diploma/",
-  },
-  {
-    text: "2024-жылдын 22-майындагы № 258 токтому. ИМА жөнүндө ЖОБО",
-    link: "https://cbd.minjust.gov.kg/46-3664/edition/8733/ru",
-  },
-  {
-    text: "2025-жылдын 10-июну КР Министрлер Кабинетинин № 329 Кесиптик жогорку жана орто билим берүү жаатындагы ченемдик укуктук актыларды бекитүү жөнүндө токтому.",
-    link: "https://cbd.minjust.gov.kg/7-43071/edition/34437/kg",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2025-жылдын 10-июнундагы № 329 токтомуна 1-тиркеме. Кыргыз Республикасынын кесиптик жогорку жана жогорку окуу жайынан кийинки билим берүү уюму жөнүндө ЖОБО.",
-    link: "https://cbd.minjust.gov.kg/230029680/edition/35047/kg?lang=kg",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2025-жылдын 10-июнундагы № 329 токтомуна 2-тиркеме. Кыргыз Республикасынын кесиптик жогорку жана орто билим берүүсүнүн мамлекеттик билим берүү стандарттары жөнүндө ЖОБО.",
-    link: "https://cbd.minjust.gov.kg/230029723/edition/35177/kg?lang=kg",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2025-жылдын 10-июнундагы № 329 токтомуна 3-тиркеме. Кыргыз Республикасынын кесиптик жогорку жана орто билим берүүнүн окуу-методикалык бирикмеси жөнүндө ЖОБО.",
-    link: "https://cbd.minjust.gov.kg/230029742/edition/35178/kg?lang=kg",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2025-жылдын 10-июнундагы № 329 токтомуна 4-тиркеме. Кыргыз Республикасынын кесиптик жогорку билим берүү уюмунун факультети жана кафедрасы жөнүндө ЖОБО.",
-    link: "https://cbd.minjust.gov.kg/230029762/edition/35180/kg?lang=kg",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2025-жылдын 10-июнундагы № 329 токтомуна 5-тиркеме. Кыргыз Республикасынын кесиптик жогорку билим берүү уюмунун окумуштуулар кеңеши жөнүндө ЖОБО.",
-    link: "https://cbd.minjust.gov.kg/230029770/edition/35181/kg?lang=kg",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2025-жылдын 10-июнундагы № 329 токтомуна 6-тиркеме. Кыргыз Республикасынын кесиптик жогорку билим берүү уюмдарында профессордук-окутуучулук курамдын кызмат орундарын ээлөө тартиби жөнүндө ЖОБО.",
-    link: "https://cbd.minjust.gov.kg/230029826/edition/35182/kg?lang=k",
-  },
-  {
-    text: "КР Министрлер Кабинетинин 2025-жылдын 10-июнундагы № 329 токтомуна 7-тиркеме. Кыргыз Республикасында кесиптик кошумча билим берүү жөнүндө ЖОБО.",
-    link: "https://cbd.minjust.gov.kg/230029729/edition/35183/kg?lang=kg",
-  },
-  {
-    text: "Постановление №231 от 10 мая 2024 года Кыргызской Республики. О порядке финансирования государственных образовательных организация НПО, СПО и ВПО.",
-    link: "https://drive.google.com/file/d/10IJ84Fae5tIlB9Pwn0W_mG4hOyqK6I-Z/view",
-  },
-];
-
 export default function NLAKyrgyzRepublic() {
   const { t } = useTranslation();
+  const documents = t("documentNLA.documents", { returnObjects: true });
 
   return (
     <section className="container mx-auto px-4 sm:px-6 lg:px-20 py-3">
       <h1 className="font-serif text-primary text-2xl sm:text-3xl md:text-4xl text-center py-6 md:py-9 uppercase italic">
-        НПА КР
+        {t("documentNLA.title")}
       </h1>
 
       <div className="md:hidden space-y-4">
-        {STATIC_ITEMS.map((item, index) => (
+        {documents.map((item, index) => (
           <div
             key={index}
             className="bg-white border border-black rounded-lg p-4 shadow-sm"
@@ -96,7 +30,6 @@ export default function NLAKyrgyzRepublic() {
                 variant="secondary"
                 onClick={() => window.open(item.link, "_blank")}
                 className="px-4 py-2 text-sm w-full sm:w-auto"
-                fullWidth
               >
                 {t("documentNLA.btnText")}
               </Button>
@@ -108,7 +41,7 @@ export default function NLAKyrgyzRepublic() {
       <div className="hidden md:block overflow-x-auto">
         <table className="min-w-full border border-black">
           <tbody>
-            {STATIC_ITEMS.map((item, index) => (
+            {documents.map((item, index) => (
               <tr
                 key={index}
                 className="border-b border-black last:border-b-0 hover:bg-gray-50 transition-colors"
@@ -116,9 +49,7 @@ export default function NLAKyrgyzRepublic() {
                 <td className="w-16 px-4 py-3 text-center text-base font-medium text-gray-900 border-r border-black">
                   {index + 1}
                 </td>
-                <td className="px-4 py-3 text-base text-gray-700">
-                  {item.text}
-                </td>
+                <td className="px-4 py-3 text-base text-gray-700">{item.text}</td>
                 <td className="w-48 px-4 py-3">
                   <div className="flex justify-end">
                     <Button
--- src/components/NLAkguki.jsx
+++ src/components/NLAkguki.jsx
@@ -1,63 +1,92 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Button from "../components/UI/Button";
 
-const NLA_KGUKI_ITEMS = [
-  "Постановление Совета Министров Киргизской ССР № 482 от 30 августа 1967 года «Об организации Киргизского государственного института искусств» (КГИИ).",
-  "Постановление Совета Министров Киргизской ССР № 21 от 28 января 1974 года, об официальном наименовании КГИИ, как «Киргизский государственный институт искусств имени Бубусары Бейшеналиевой».",
-  "Постановление Правительства КР №470 от 28.08.2013 года, о преобразовании Кыргызского государственного института искусств имени Б.Бейшеналиевой в Кыргызский государственный университет культуры и искусств имени Бубусары Бейшеналиевой.",
-  "КРнын Президентинин Жарлыгы. Бубусара Бейшеналиева атындагы Кыргыз мамлекеттик маданият жана искусство университетине “улуттук” статусту ыйгаруу жөнүндө. №96, 22.03.2025.",
-  "Кайра каттоо күбөлүгү / Свидетельство о государственной перерегистрации (2025)",
-  "КУМИУнун Уставы (2025) / Устав КНУКИ (2025);",
-  "Б.Бейшеналиева атындагы Кыргыз улуттук маданият жана искусство университети жөнүндө ЖОБО.",
-  "Б.Бейшеналиева атындагы Кыргыз улуттук маданият жана искусство университетинин окуу-усулдук бирикмеси жөнүндөгү ЖОБОсу",
-  "Б.Бейшеналиева атындагы Кыргыз улуттук маданият жана искусство университетинин факультет жана кафедра жөнүндө ЖОБОсу",
-  "Б. Бейшеналиева атындагы Кыргыз улуттук маданият жана искусство университетинин Окумуштуулар Кеңеши жөнүндөгү ЖОБОсу",
-  "Б.Бейшеналиева атындагы Кыргыз улуттук маданият жана искусство университетинин профессордук-окутуучулук курамдын кызмат орундарын ээлөө тартиби жөнүндөгү ЖОБОсу",
-  "Б.Бейшеналиева атындагы КУМИУда академиялык кредиттерди колдонуу менен окуу процессин уюштуруу жөнүндөгү ЖОБО",
-  "Б.Бейшеналиева атындагы Кыргыз улуттук маданият жана искусство университетинде билим алуучуларын которуу, окуудан чыгаруу, окууга калыбына келтирүү жана академиялык өргүү берүү тартиби жөнүндөгү ЖОБО",
-  "Б.Бейшеналиева атындагы Кыргыз улуттук маданият жана искусство университетинде окутуунун модулдук-рейтингдик технологиясын колдонуу боюнча НУСКАМА (И Н С Т Р У К Ц И Я)",
-  "Бейшеналиева ат. КУМИУнин коррупцияга каршы саясаты жөнүндөгү ЖОБОсу.",
-  'КЫРГЫЗ РЕСПУБЛИКАСЫНЫН ЖОГОРКУ КЕСИПТИК БИЛИМ БЕРҮҮНҮН МАМЛЕКЕТТИК БИЛИМ БЕРҮҮ СТАНДАРТЫ. Квалификация: "Специалист" (2021-жылдын 11-декабрындагы №1578/1 буйругу менен бекитилген.) 570002 Театр таануу 570027 Адабий чыгармачылык 570013 Үн режиссурасы (колдонулуучу багыттары боюнча) 570014 Актердук өнөр 570015 Режиссура (колдонулуучу багыттары боюнча) 570006 Кинооператорлук 570029 Социалдык-маданий ишмердүүлүк 570019 Хореография педагогикасы 550600 Көркөмдүк билим берүү',
-  'КЫРГЫЗ РЕСПУБЛИКАСЫНЫН ЖОГОРКУ КЕСИПТИК БИЛИМ БЕРҮҮНҮН МАМЛЕКЕТТИК БИЛИМ БЕРҮҮ СТАНДАРТЫ. Квалификация: "магистр" (2024-жылдын 21-сентябрындагы №1815/1 буйругу менен бекитилген.) 570200 Музыкалык искусство 570300 Театралдык искусство 550600 Көркөмдүк билим берүү',
-  "Положение об основной образовательной программе (ООП) Кыргызского национального университета культуры и искусств им. Б. Бейшеналиевой",
-  "КУМИУнун бүтүрүүчүлөрүн жыйынтыктоочу мам.аттестациялоо ж-до Жобосу",
-  "Положение о документах о среднем и высшем профессиональном образовании государственного образца, порядке их изготовления, оплаты, хранения, выдачи и учета. Приложение к приказу МОН КР от 10.03.2025 года №249/1.",
-  "Положение о ящике доверия КНУКИ им. Б.Бейшеналиевой",
-  "Положение об отделе качества образования, лицензирования и аккредитации Кыргызского национального университета культуры и искусств им. Б. Бейшеналиевой",
-  "Положение об УМК",
-  "Положение об академической мобильности",
-  "Б.БЕЙШЕНАЛИЕВА АТ. КУМИУнун МУЗЫКАЛЫК АСПАПТАРДЫ САКТОО ЖӨНҮНДӨ ЖОБОСУ",
-  "Положение о мониторинге удовлетворенности качеством образования потребителей КНУКИ им. Б.Бейшеналиевой",
-  "Университеттин практиканы уюштурууга жана өткөрүү жөнүндө жобосу",
-  "Магистрантдардын практикаларды өтөө жөнүндө жобо",
-  "МАГИСТРДИК ДИССЕРТАЦИЯ БОЮНЧА ЖОБО",
-  "Б.Бейшеналиева атындагы Кыргыз улуттук маданият жана искусство университетинде ун эмгекти коргоо жана билим беруу процесссинде коопсуз шарттарды камсыз кылуу тууралуу ЖОБО",
-  "Б.Бейшеналиева атындагы Кыргыз улуттук маданият жана искусство университетиндеги студенттердин тандоо курстары жөнүндө ЖОБО",
-];
+import doc1 from "../assets/pdf/doc1.pdf";
+import doc2 from "../assets/pdf/doc2.pdf";
+import doc3 from "../assets/pdf/doc3.pdf";
+import doc4 from "../assets/pdf/doc4.pdf";
+import doc5 from "../assets/pdf/doc5.pdf";
+import doc6 from "../assets/pdf/doc6.pdf";
+import doc7 from "../assets/pdf/doc7.pdf";
+import doc8 from "../assets/pdf/doc8.pdf";
+import doc9 from "../assets/pdf/doc9.pdf";
+import doc10 from "../assets/pdf/doc10.pdf";
+import doc11 from "../assets/pdf/doc11.pdf";
+import doc12 from "../assets/pdf/doc12.pdf";
+import doc13 from "../assets/pdf/doc13.pdf";
+import doc14 from "../assets/pdf/doc14.pdf";
+import doc15 from "../assets/pdf/doc15.pdf";
+import doc16 from "../assets/pdf/doc16.pdf";
+import doc17 from "../assets/pdf/doc17.pdf";
+import doc18 from "../assets/pdf/doc18.pdf";
+import doc19 from "../assets/pdf/doc19.pdf";
+import doc20 from "../assets/pdf/doc20.pdf";
+import doc21 from "../assets/pdf/doc21.pdf";
+import doc22 from "../assets/pdf/doc22.pdf";
+import doc23 from "../assets/pdf/doc23.pdf";
+import doc24 from "../assets/pdf/doc24.pdf";
+import doc25 from "../assets/pdf/doc25.pdf";
+import doc26 from "../assets/pdf/doc26.pdf";
+import doc27 from "../assets/pdf/doc27.pdf";
+import doc28 from "../assets/pdf/doc28.pdf";
+import doc29 from "../assets/pdf/doc29.pdf";
+import doc30 from "../assets/pdf/doc30.pdf";
+import doc31 from "../assets/pdf/doc31.pdf";
+import doc32 from "../assets/pdf/doc32.pdf";
+
+const nlaKgukiPdfs = {
+  doc1, doc2, doc3, doc4, doc5, doc6, doc7, doc8, doc9, doc10, doc11, doc12, doc13, doc14, doc15, doc16, doc17, doc18, doc19, doc20, doc21, doc22, doc23, doc24, doc25,
+  doc26, doc27, doc28, doc29, doc30, doc31, doc32,
+};
+
+function PdfModal({ pdf, onClose }) {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+      <div className="bg-white w-11/12 md:w-3/4 h-5/6 rounded-lg overflow-hidden relative">
+        <button
+          className="absolute top-2 right-2 bg-gray-200 px-3 py-1 rounded"
+          onClick={onClose}
+        >
+          x
+        </button>
+        <iframe
+          src={pdf}
+          className="w-full h-full"
+          title="Документ"
+        />
+      </div>
+    </div>
+  );
+}
 
 export default function NLAkguki() {
   const { t } = useTranslation();
+  const documents = t("NLAkguki.documents", { returnObjects: true });
+  const [activePdf, setActivePdf] = useState(null);
 
   return (
     <section className="container mx-auto px-4 sm:px-6 lg:px-20 py-4 sm:py-8">
       <h1 className="font-serif text-primary text-2xl sm:text-3xl md:text-4xl text-center py-6 sm:py-9 uppercase italic">
-        НПА КНУКИ
+        {t("NLAkguki.title")}
       </h1>
 
       <div className="block md:hidden space-y-4">
-        {NLA_KGUKI_ITEMS.map((text, index) => (
-          <div key={index} className="bg-white border border-black rounded-lg p-4">
-            <div className="flex items-start gap-3 mb-3">
-              <span className="flex-shrink-0 w-8 h-8 flex items-center justify-center bg-gray-100 rounded text-sm font-medium">
+        {documents.map((item, index) => (
+          <div key={item.id} className="bg-white border border-black rounded-lg p-4">
+            <div className="flex items-start gap-3 mb-4">
+              <span className="w-8 h-8 flex items-center justify-center bg-gray-100 rounded text-sm font-medium">
                 {index + 1}
               </span>
-              <p className="text-sm text-gray-700">{text}</p>
+              <p className="text-sm text-gray-700">{item.text}</p>
             </div>
-            <div className="flex justify-end">
-              <Button variant="secondary" className="px-4 py-2 text-sm w-full">
-                {t("documentNLA.btnText")}
-              </Button>
-            </div>
+            <Button
+              variant="secondary"
+              className="w-full px-4 py-2 text-sm"
+              onClick={() => setActivePdf(nlaKgukiPdfs[item.id])}
+            >
+              {t("NLAkguki.btnText")}
+            </Button>
           </div>
         ))}
       </div>
@@ -65,18 +94,20 @@ export default function NLAkguki() {
       <div className="hidden md:block overflow-x-auto">
         <table className="min-w-full border border-black">
           <tbody>
-            {NLA_KGUKI_ITEMS.map((text, index) => (
-              <tr key={index} className="border-b border-black last:border-b-0">
-                <td className="w-16 px-4 py-3 text-center text-base font-medium text-gray-900 border-r border-black">
+            {documents.map((item, index) => (
+              <tr key={item.id} className="border-b border-black last:border-b-0">
+                <td className="w-16 px-4 py-3 text-center font-medium border-r border-black">
                   {index + 1}
                 </td>
-                <td className="px-4 py-3 text-base text-gray-700">
-                  {text}
-                </td>
+                <td className="px-4 py-3 text-gray-700">{item.text}</td>
                 <td className="w-48 px-4 py-3">
                   <div className="flex justify-end">
-                    <Button variant="secondary" className="px-6 py-2 text-sm">
-                      {t("documentNLA.btnText")}
+                    <Button
+                      variant="secondary"
+                      className="px-6 py-2 text-sm"
+                      onClick={() => setActivePdf(nlaKgukiPdfs[item.id])}
+                    >
+                      {t("NLAkguki.btnText")}
                     </Button>
                   </div>
                 </td>
@@ -85,6 +116,8 @@ export default function NLAkguki() {
           </tbody>
         </table>
       </div>
+
+      {activePdf && <PdfModal pdf={activePdf} onClose={() => setActivePdf(null)} />}
     </section>
   );
 }
