7201eb89508a7409ad5281a709123e5a208b3bfc
Islam <gojo060809@gmail.com>
2026-01-17 16:41:18 +0600

    accourding3 and according4

--- src/pages/Accounting.jsx
+++ src/pages/Accounting.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import AccountingFirst from "../components/AccountingFirst.jsx";
+import AccountingFourth from "../components/AccountingFourth.jsx";
 import AccountingSecond from "../components/AccountingSecond.jsx";
+import AccountingThird from "../components/AccountingThird.jsx";
 function Accounting() {
   return (
     <div>
       <AccountingFirst />
       <AccountingSecond />
+      <AccountingThird />
+      <AccountingFourth />
     </div>
   );
 }
