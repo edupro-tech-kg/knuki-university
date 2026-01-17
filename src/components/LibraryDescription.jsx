c90aff4231c19871eb38837e4449fbfa87463160
Саадат <saadat@MacBook-Air-Saadat.local>
2026-01-17 15:31:23 +0600

    library: update info inside, added new section

--- src/components/LibraryDescription.jsx
+++ src/components/LibraryDescription.jsx
@@ -3,7 +3,6 @@ import { useTranslation } from "react-i18next";
 
 function LibraryDescription() {
   const { t } = useTranslation();
-
   const library = t("libraryDescription", { returnObjects: true });
 
   return (
@@ -12,14 +11,22 @@ function LibraryDescription() {
         <h2 className="uppercase font-serif text-4xl font-bold mb-4 text-primary italic">
           {library.title}
         </h2>
-        <p className="text-text-primary text-left">{library.content}</p>
+
+        <p className="text-text-primary text-left whitespace-pre-line">
+          {library.content}
+        </p>
       </div>
 
       <div className="grid gap-6 md:grid-cols-2">
         {library.cards.map((card) => (
-          <div key={card.id} className="p-3 border rounded-lg">
-            <h4 className="text-lg text-text-primary font-bold mb-2">{card.name}</h4>
-            <p className="text-text-primary">{card.description}</p>
+          <div key={card.id} className="p-4 border rounded-lg">
+            <h4 className="text-lg text-text-primary font-bold mb-2">
+              {card.name}
+            </h4>
+
+            <p className="text-text-primary whitespace-pre-line">
+              {card.description}
+            </p>
           </div>
         ))}
       </div>
